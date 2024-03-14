330493e8de6ddb9ac16660bdc5123b2fddcf91a6
nvrm0r <nvrm0r>
2024-03-14 16:50:28 +0300

    v0.9

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <sdbusplus/bus.hpp>
 #include <chrono>
 #include <thread>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include <iostream>
 
@@ -13,6 +16,7 @@ const std::string demoServiceName = "inmys-uart.service";
 const std::string demoObjectPath = "/nms/bklt";
 const std::string demoInterfaceName = "nms.bklt";
 const std::string S5Pin = "PWR_S5";
+const char* BMC_PATH = "/dev/ttyACM0";
 
 class Application
 {
@@ -27,7 +31,11 @@ class Application
             [this](sdbusplus::asio::dbus_interface& demo) {
             demo.register_property_r<bool>(
                 S5Pin, sdbusplus::vtable::property_::emits_change,
-                [this](bool) { return greetings_; });
+                [this](bool) { 
+                    unsigned char reg = readRegister(BMC_PATH);
+                    setState(reg & 1<<0);
+                    return _S3_value; 
+                    });
         });
     }
 
@@ -55,26 +63,12 @@ class Application
                 getFailed();
                 return;
             }
-            std::cout << "S5 value is: " << value << "\n";
+            std::cout << "S3 value is: " << value << "\n";
         });
     }
 
-    void asyncChangeProperty()
-    {
-        sdbusplus::asio::setProperty(
-            bus_, demoServiceName, demoObjectPath, demoInterfaceName,
-            S5Pin, true,
-            [this](const boost::system::error_code& ec) {
-            if (ec)
-            {
-                getFailed();
-                return;
-            }
-
-            std::cout << "Works: Here\n";
-            ++fatalErrors_;
-        });
-
+    void setState(bool newState){
+        _S3_value = newState;
     }
 
   private:
@@ -83,9 +77,51 @@ class Application
     sdbusplus::asio::connection& bus_;
 
     std::unique_ptr<sdbusplus::asio::dbus_interface> demo_;
-    bool greetings_ = false;
+    bool _S3_value = false;
 
     uint32_t fatalErrors_ = 0u;
+
+    int readRegister(const char* dev){
+        int fd = open(dev, O_RDWR);
+
+        if (fd == -1) {
+            std::cerr << "Error opening " << dev << std::endl;
+            return -1;
+        }
+
+        struct termios tty;
+        tcgetattr(fd, &tty);
+        cfsetspeed(&tty, B115200);
+        cfmakeraw(&tty);
+
+        tty.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHOCTL | ECHOKE);
+
+        tty.c_cc[VTIME] = 10;
+        tty.c_cc[VMIN] = 0;
+        tcsetattr(fd, TCSANOW, &tty);
+
+        unsigned char txByte[] =   {'0','0','r','b','0','1','\r','\n'};
+        unsigned char rxByte[10] = {0,0,0,0,0,0,0,0,0,0};
+        int len;
+        // Send byte
+        if ((len = write(fd, txByte, 8)) == -1) {
+            std::cerr << "Error writing to serial port" << std::endl;
+            return -1;
+        }
+        // Receive byte
+        if ((len = read(fd, rxByte, 7)) == -1) {
+            std::cerr << "Error reading from serial port" << std::endl;
+            return -1;
+        }
+        close(fd);
+        printf("Read: %s",rxByte);
+        unsigned char Hb = (rxByte[5] >= 'A') ? (rxByte[5] - 'A' + 10) : (rxByte[5] - '0');
+        unsigned char Lb = (rxByte[6] >= 'A') ? (rxByte[6] - 'A' + 10) : (rxByte[6] - '0');
+        unsigned char result = Hb*10+Lb;
+        return result;
+    }
+
+
 };
 
 int main(int, char**)
@@ -104,10 +140,11 @@ int main(int, char**)
     Application app(ioc, *bus, *objServer);
 
 
-    boost::asio::post(ioc, [&app] { app.asyncReadProperties(); });
     std::cout << "Trying to change: \n";
-    boost::asio::post(ioc, [&app] { app.asyncChangeProperty(); });
+    
+    boost::asio::post(ioc, [&app] { app.asyncReadProperties(); });
 
+    app.setState(true);
 
 
     ioc.run();
